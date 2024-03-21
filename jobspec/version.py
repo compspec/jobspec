927fdee3333ae0084070ec67d4a1be32b8465558
vsoch <vsoch@users.noreply.github.com>
2024-03-21 02:10:17 -0600

    tweaks to schema
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- jobspec/version.py
+++ jobspec/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 AUTHOR = "Vanessa Sochat"
 AUTHOR_EMAIL = "vsoch@users.noreply.github.com"
 NAME = "jobspec"
@@ -11,7 +11,7 @@ LICENSE = "LICENSE"
 ################################################################################
 # Global requirements
 
-INSTALL_REQUIRES = ()
+INSTALL_REQUIRES = (("jsonschema", {"min_version": None}),)
 
 TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
 INSTALL_REQUIRES_ALL = INSTALL_REQUIRES + TESTS_REQUIRES
